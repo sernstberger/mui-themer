9dc42baeef5ebbf4653bcbac139a25948e0c5987
Steve Ernstberger <steve32285@gmail.com>
2024-10-14 10:21:52 -0400

    added a11y

--- mui-themer/src/app/app.tsx
+++ mui-themer/src/app/app.tsx
@@ -2,71 +2,73 @@ import React from 'react';
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 import { Box, Typography } from '@mui/material';
 
-// Define the color swatches
+// Define the color swatches with accessibility scores
 const primaryColors = [
-  { label: '25', color: '#FCFAFF' },
-  { label: '50', color: '#F9F5FF' },
-  { label: '100', color: '#F4EBFF' },
-  { label: '200', color: '#E9D7FE' },
-  { label: '300', color: '#D6BBFB' },
-  { label: '400', color: '#B692F6' },
-  { label: '500', color: '#9E77ED' },
-  { label: '600', color: '#7F56D9' },
-  { label: '700', color: '#6941C6' },
-  { label: '800', color: '#53389E' },
-  { label: '900', color: '#42307D' },
-  { label: '950', color: '#2C1C5F' },
+  { label: '25', color: '#FCFAFF', accessibility: 'AA 6.38' },
+  { label: '50', color: '#F9F5FF', accessibility: 'AA 6.15' },
+  { label: '100', color: '#F4EBFF', accessibility: 'AA 5.72' },
+  { label: '200', color: '#E9D7FE', accessibility: 'AA 4.92' },
+  { label: '300', color: '#D6BBFB', accessibility: '3.89' },
+  { label: '400', color: '#B692F6', accessibility: '2.49' },
+  { label: '500', color: '#9E77ED', accessibility: '3.32' },
+  { label: '600', color: '#7F56D9', accessibility: 'AA 4.95' },
+  { label: '700', color: '#6941C6', accessibility: 'AA 6.61' },
+  { label: '800', color: '#53389E', accessibility: 'AAA 8.65' },
+  { label: '900', color: '#42307D', accessibility: 'AAA 10.76' },
+  { label: '950', color: '#2C1C5F', accessibility: 'AAA 14.59' },
 ];
 
 const errorColors = [
-  { label: '25', color: '#FFFBFA' },
-  { label: '50', color: '#FEF3F2' },
-  { label: '100', color: '#FEE4E2' },
-  { label: '200', color: '#FECDCA' },
-  { label: '300', color: '#FDA29B' },
-  { label: '400', color: '#F97066' },
-  { label: '500', color: '#F04438' },
-  { label: '600', color: '#D92D20' },
-  { label: '700', color: '#B42318' },
-  { label: '800', color: '#912018' },
-  { label: '900', color: '#7A271A' },
-  { label: '950', color: '#55160C' },
+  { label: '25', color: '#FFFBFA', accessibility: 'AA 6.39' },
+  { label: '50', color: '#FEF3F2', accessibility: 'AA 6.04' },
+  { label: '100', color: '#FEE4E2', accessibility: 'AA 5.45' },
+  { label: '200', color: '#FECDCA', accessibility: 'AA 4.63' },
+  { label: '300', color: '#FDA29B', accessibility: '3.38' },
+  { label: '400', color: '#F97066', accessibility: '2.78' },
+  { label: '500', color: '#F04438', accessibility: '3.75' },
+  { label: '600', color: '#D92D20', accessibility: 'AA 4.83' },
+  { label: '700', color: '#B42318', accessibility: 'AA 6.57' },
+  { label: '800', color: '#912018', accessibility: 'AAA 8.66' },
+  { label: '900', color: '#7A271A', accessibility: 'AAA 9.84' },
+  { label: '950', color: '#55160C', accessibility: 'AAA 13.94' },
 ];
 
 const warningColors = [
-  { label: '25', color: '#FFFCF5' },
-  { label: '50', color: '#FFFAEB' },
-  { label: '100', color: '#FEF0C7' },
-  { label: '200', color: '#FEDF89' },
-  { label: '300', color: '#FEC84B' },
-  { label: '400', color: '#FDB022' },
-  { label: '500', color: '#F79009' },
-  { label: '600', color: '#DC6803' },
-  { label: '700', color: '#B54708' },
-  { label: '800', color: '#93370D' },
-  { label: '900', color: '#7A2E0E' },
-  { label: '950', color: '#4E1D09' },
+  { label: '25', color: '#FFFCF5', accessibility: 'AA 5.29' },
+  { label: '50', color: '#FFFAEB', accessibility: 'AA 5.20' },
+  { label: '100', color: '#FEF0C7', accessibility: 'AA 4.78' },
+  { label: '200', color: '#FEDF89', accessibility: '4.16' },
+  { label: '300', color: '#FEC84B', accessibility: '3.50' },
+  { label: '400', color: '#FDB022', accessibility: '1.84' },
+  { label: '500', color: '#F79009', accessibility: '2.34' },
+  { label: '600', color: '#DC6803', accessibility: 'AA 3.48' },
+  { label: '700', color: '#B54708', accessibility: 'AA 5.42' },
+  { label: '800', color: '#93370D', accessibility: 'AAA 7.51' },
+  { label: '900', color: '#7A2E0E', accessibility: 'AAA 9.43' },
+  { label: '950', color: '#4E1D09', accessibility: 'AAA 13.96' },
 ];
 
 const successColors = [
-  { label: '25', color: '#F6FEF9' },
-  { label: '50', color: '#ECFDF3' },
-  { label: '100', color: '#DCFAE6' },
-  { label: '200', color: '#ABEFC6' },
-  { label: '300', color: '#75E0A7' },
-  { label: '400', color: '#47CD89' },
-  { label: '500', color: '#17B26A' },
-  { label: '600', color: '#079455' },
-  { label: '700', color: '#067647' },
-  { label: '800', color: '#085D3A' },
-  { label: '900', color: '#074D31' },
-  { label: '950', color: '#053321' },
+  { label: '25', color: '#F6FEF9', accessibility: 'AA 5.54' },
+  { label: '50', color: '#ECFDF3', accessibility: 'AA 5.39' },
+  { label: '100', color: '#DCFAE6', accessibility: 'AA 5.11' },
+  { label: '200', color: '#ABEFC6', accessibility: '4.29' },
+  { label: '300', color: '#75E0A7', accessibility: '3.51' },
+  { label: '400', color: '#47CD89', accessibility: '2.02' },
+  { label: '500', color: '#17B26A', accessibility: '2.75' },
+  { label: '600', color: '#079455', accessibility: 'AA 3.90' },
+  { label: '700', color: '#067647', accessibility: 'AA 5.69' },
+  { label: '800', color: '#085D3A', accessibility: 'AAA 7.96' },
+  { label: '900', color: '#074D31', accessibility: 'AAA 9.92' },
+  { label: '950', color: '#053321', accessibility: 'AAA 13.98' },
 ];
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#7F56D9',
+      main:
+        primaryColors.find((color) => color.label === '600')?.color ||
+        '#7F56D9',
     },
   },
   typography: {
@@ -74,7 +76,15 @@ const theme = createTheme({
   },
 });
 
-const ColorSwatch = ({ label, color }: { label: string; color: string }) => (
+const ColorSwatch = ({
+  label,
+  color,
+  accessibility,
+}: {
+  label: string;
+  color: string;
+  accessibility: string;
+}) => (
   <Box
     sx={{
       backgroundColor: color,
@@ -82,6 +92,7 @@ const ColorSwatch = ({ label, color }: { label: string; color: string }) => (
       height: 100,
       margin: 1,
       display: 'flex',
+      flexDirection: 'column',
       alignItems: 'center',
       justifyContent: 'center',
       borderRadius: 1,
@@ -89,6 +100,9 @@ const ColorSwatch = ({ label, color }: { label: string; color: string }) => (
   >
     <Typography variant="body2" color="#fff">
       {label}
+    </Typography>
+    <Typography variant="caption" color="#fff">
+      {accessibility}
     </Typography>
   </Box>
 );
@@ -110,6 +124,7 @@ const App: React.FC = () => {
               key={swatch.label}
               label={swatch.label}
               color={swatch.color}
+              accessibility={swatch.accessibility}
             />
           ))}
         </Box>
@@ -122,6 +137,7 @@ const App: React.FC = () => {
               key={swatch.label}
               label={swatch.label}
               color={swatch.color}
+              accessibility={swatch.accessibility}
             />
           ))}
         </Box>
@@ -134,6 +150,7 @@ const App: React.FC = () => {
               key={swatch.label}
               label={swatch.label}
               color={swatch.color}
+              accessibility={swatch.accessibility}
             />
           ))}
         </Box>
@@ -146,6 +163,7 @@ const App: React.FC = () => {
               key={swatch.label}
               label={swatch.label}
               color={swatch.color}
+              accessibility={swatch.accessibility}
             />
           ))}
         </Box>
