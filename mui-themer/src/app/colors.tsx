a01d981299396b6e3b6a6d035ff64b2e56b4e648
Steve Ernstberger <steve32285@gmail.com>
2024-10-14 11:00:09 -0400

    added grays

--- mui-themer/src/app/colors.tsx
+++ mui-themer/src/app/colors.tsx
@@ -57,3 +57,78 @@ export const success = [
   { label: '900', color: '#074D31', accessibility: 'AAA 9.92' },
   { label: '950', color: '#053321', accessibility: 'AAA 13.98' },
 ];
+
+export const grayBlue = [
+  { label: '25', color: '#FCFCFD', accessibility: 'AAA 8.39' },
+  { label: '50', color: '#F8F9FC', accessibility: 'AAA 8.17' },
+  { label: '100', color: '#EAECF5', accessibility: 'AAA 7.30' },
+  { label: '200', color: '#D5D9EB', accessibility: 'AA 6.13' },
+  { label: '300', color: '#B3B8DB', accessibility: '4.42' },
+  { label: '400', color: '#717BBC', accessibility: '3.99' },
+  { label: '500', color: '#4E5BA6', accessibility: 'AA 6.22' },
+  { label: '600', color: '#3E4784', accessibility: 'AAA 8.60' },
+  { label: '700', color: '#363F72', accessibility: 'AAA 9.94' },
+  { label: '800', color: '#293056', accessibility: 'AAA 12.71' },
+  { label: '900', color: '#101323', accessibility: 'AAA 18.42' },
+  { label: '950', color: '#0D0F1C', accessibility: 'AAA 19.05' },
+];
+
+export const grayCool = [
+  { label: '25', color: '#FCFCFD', accessibility: 'AAA 7.15' },
+  { label: '50', color: '#F9F9FB', accessibility: 'AA 6.97' },
+  { label: '100', color: '#EFF1F5', accessibility: 'AA 5.51' },
+  { label: '200', color: '#DCDFEA', accessibility: '4.03' },
+  { label: '300', color: '#B9C0D4', accessibility: '3.45' },
+  { label: '400', color: '#7D89B0', accessibility: 'AA 5.22' },
+  { label: '500', color: '#5D6B98', accessibility: 'AAA 7.33' },
+  { label: '600', color: '#4A5578', accessibility: 'AAA 8.85' },
+  { label: '700', color: '#404968', accessibility: 'AAA 11.76' },
+  { label: '800', color: '#30374F', accessibility: 'AAA 18.41' },
+  { label: '900', color: '#111322', accessibility: 'AAA 18.94' },
+  { label: '950', color: '#0E101B', accessibility: 'AAA 19.05' },
+];
+
+export const grayModern = [
+  { label: '25', color: '#FCFCFC', accessibility: 'AAA 8.05' },
+  { label: '50', color: '#F9F9F9', accessibility: 'AAA 7.92' },
+  { label: '100', color: '#F3F3F3', accessibility: 'AA 6.85' },
+  { label: '200', color: '#E6E6E6', accessibility: 'AA 5.49' },
+  { label: '300', color: '#CCCCCC', accessibility: '4.00' },
+  { label: '400', color: '#999999', accessibility: '3.52' },
+  { label: '500', color: '#666666', accessibility: 'AAA 7.22' },
+  { label: '600', color: '#4D4D4D', accessibility: 'AAA 9.08' },
+  { label: '700', color: '#333333', accessibility: 'AAA 13.15' },
+  { label: '800', color: '#1A1A1A', accessibility: 'AAA 18.25' },
+  { label: '900', color: '#0D0D0D', accessibility: 'AAA 19.05' },
+  { label: '950', color: '#050505', accessibility: 'AAA 19.05' },
+];
+
+export const grayNeutral = [
+  { label: '25', color: '#FCFCFC', accessibility: 'AAA 7.86' },
+  { label: '50', color: '#FAFAFA', accessibility: 'AAA 7.73' },
+  { label: '100', color: '#F5F5F5', accessibility: 'AA 6.52' },
+  { label: '200', color: '#EAEAEA', accessibility: '5.31' },
+  { label: '300', color: '#D8D8D8', accessibility: '4.28' },
+  { label: '400', color: '#A6A6A6', accessibility: '3.22' },
+  { label: '500', color: '#737373', accessibility: 'AAA 6.67' },
+  { label: '600', color: '#595959', accessibility: 'AAA 8.94' },
+  { label: '700', color: '#404040', accessibility: 'AAA 12.96' },
+  { label: '800', color: '#262626', accessibility: 'AAA 17.93' },
+  { label: '900', color: '#171717', accessibility: 'AAA 19.05' },
+  { label: '950', color: '#0F0F0F', accessibility: 'AAA 19.05' },
+];
+
+export const grayIron = [
+  { label: '25', color: '#FAFAFB', accessibility: 'AAA 7.92' },
+  { label: '50', color: '#F5F5F7', accessibility: 'AAA 7.76' },
+  { label: '100', color: '#E8E8EB', accessibility: 'AA 6.33' },
+  { label: '200', color: '#D1D1D7', accessibility: '4.94' },
+  { label: '300', color: '#B5B5C2', accessibility: '4.11' },
+  { label: '400', color: '#8C8CA8', accessibility: 'AA 5.06' },
+  { label: '500', color: '#666687', accessibility: 'AAA 7.52' },
+  { label: '600', color: '#4D4D66', accessibility: 'AAA 9.33' },
+  { label: '700', color: '#3A3A52', accessibility: 'AAA 11.57' },
+  { label: '800', color: '#2D2D3E', accessibility: 'AAA 14.81' },
+  { label: '900', color: '#1E1E28', accessibility: 'AAA 19.05' },
+  { label: '950', color: '#15151F', accessibility: 'AAA 19.05' },
+];
