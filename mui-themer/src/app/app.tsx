d9c309d3e0d08715966fb015ca9e365e985edaad
Steve Ernstberger <steve32285@gmail.com>
2024-10-14 13:39:31 -0400

    added typography

--- mui-themer/src/app/app.tsx
+++ mui-themer/src/app/app.tsx
@@ -23,6 +23,60 @@ import {
 } from './colors';
 import { ColorSwatchList } from './Components/ColorSwatchList';
 
+export function Types() {
+  return (
+    <Box>
+      <Typography variant="h1" gutterBottom>
+        h1. Heading
+      </Typography>
+      <Typography variant="h2" gutterBottom>
+        h2. Heading
+      </Typography>
+      <Typography variant="h3" gutterBottom>
+        h3. Heading
+      </Typography>
+      <Typography variant="h4" gutterBottom>
+        h4. Heading
+      </Typography>
+      <Typography variant="h5" gutterBottom>
+        h5. Heading
+      </Typography>
+      <Typography variant="h6" gutterBottom>
+        h6. Heading
+      </Typography>
+      <Typography variant="subtitle1" gutterBottom>
+        subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Quos blanditiis tenetur
+      </Typography>
+      <Typography variant="subtitle2" gutterBottom>
+        subtitle2. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Quos blanditiis tenetur
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+        blanditiis tenetur unde suscipit, quam beatae rerum inventore
+        consectetur, neque doloribus, cupiditate numquam dignissimos laborum
+        fugiat deleniti? Eum quasi quidem quibusdam.
+      </Typography>
+      <Typography variant="body2" gutterBottom>
+        body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+        blanditiis tenetur unde suscipit, quam beatae rerum inventore
+        consectetur, neque doloribus, cupiditate numquam dignissimos laborum
+        fugiat deleniti? Eum quasi quidem quibusdam.
+      </Typography>
+      <Typography variant="button" gutterBottom sx={{ display: 'block' }}>
+        button text
+      </Typography>
+      <Typography variant="caption" gutterBottom sx={{ display: 'block' }}>
+        caption text
+      </Typography>
+      <Typography variant="overline" gutterBottom sx={{ display: 'block' }}>
+        overline text
+      </Typography>
+    </Box>
+  );
+}
+
 const colorMap = {
   primary,
   error,
@@ -90,6 +144,13 @@ const App = () => {
     typography: {
       fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
     },
+    components: {
+      MuiButtonBase: {
+        defaultProps: {
+          disableRipple: true, // No more ripple, on the whole application 💣!
+        },
+      },
+    },
   });
 
   const handleColorClick = (color) => {
@@ -113,6 +174,7 @@ const App = () => {
           />
         </Box>
       ))}
+      <Types />
     </ThemeProvider>
   );
 };
