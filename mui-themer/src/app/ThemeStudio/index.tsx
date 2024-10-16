ce7ff87a04057f6f9e9acd237927908beb764f90
Steve Ernstberger <steve32285@gmail.com>
2024-10-16 10:06:49 -0400

    added typography section

--- mui-themer/src/app/ThemeStudio/index.tsx
+++ mui-themer/src/app/ThemeStudio/index.tsx
@@ -8,14 +8,11 @@ import {
 } from '@mui/material';
 import { Box, Typography } from '@mui/material';
 import { PaletteItem } from '../Components/PaletteItem';
-import {
-  Button,
-  createTheme,
-  ThemeProvider as MuiThemeProvider,
-} from '@mui/material';
+import { createTheme, ThemeProvider as MuiThemeProvider } from '@mui/material';
 import '@fontsource/ibm-plex-mono';
 import { ContentCopy } from '@mui/icons-material';
 import { useFormContext } from 'react-hook-form';
+import { TypographySection } from './TypographySection';
 
 const drawerWidth = 240;
 
@@ -191,100 +188,10 @@ export const ThemeStudio = () => {
             <PaletteItem color="info" />
           </Stack>
           <Divider sx={{ my: 2 }} />
-          <Typography variant="h2">Typography</Typography>
-          <Box>
-            <Typography variant="h3">h1</Typography>
-            <TextField
-              label="Size"
-              variant="outlined"
-              type="number"
-              fullWidth
-              {...register('h1', { valueAsNumber: true })}
-            />
-            <TextField
-              label="Weight"
-              variant="outlined"
-              select
-              // onChange={(event) => setH1(Number(event.target.value))}
-              // defaultValue={h1}
-              fullWidth
-              {...register('h1Weight')}
-            >
-              <MenuItem value={100}>100 (thin)</MenuItem>
-              <MenuItem value={300}>300 (light)</MenuItem>
-              <MenuItem value={400}>400 (regular)</MenuItem>
-              <MenuItem value={500}>500 (medium)</MenuItem>
-              <MenuItem value={700}>700 (bold)</MenuItem>
-              <MenuItem value={900}>900 (black)</MenuItem>
-            </TextField>
-            <TextField
-              label="Line height"
-              variant="outlined"
-              type="number"
-              fullWidth
-              {...register('h1LineHeight', { valueAsNumber: true })}
-            />
-            <TextField
-              label="Letter spacing"
-              variant="outlined"
-              type="number"
-              fullWidth
-              {...register('h1LetterSpacing', { valueAsNumber: true })}
-            />
-            <Typography variant="h2" gutterBottom>
-              h2. Heading
-            </Typography>
-            <Typography variant="h3" gutterBottom>
-              h3. Heading
-            </Typography>
-            <Typography variant="h4" gutterBottom>
-              h4. Heading
-            </Typography>
-            <Typography variant="h5" gutterBottom>
-              h5. Heading
-            </Typography>
-            <Typography variant="h6" gutterBottom>
-              h6. Heading
-            </Typography>
-            <Typography variant="subtitle1" gutterBottom>
-              subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing
-              elit. Quos blanditiis tenetur
-            </Typography>
-            <Typography variant="subtitle2" gutterBottom>
-              subtitle2. Lorem ipsum dolor sit amet, consectetur adipisicing
-              elit. Quos blanditiis tenetur
-            </Typography>
-            <Typography variant="body1" gutterBottom>
-              body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore
-              consectetur, neque doloribus, cupiditate numquam dignissimos
-              laborum fugiat deleniti? Eum quasi quidem quibusdam.
-            </Typography>
-            <Typography variant="body2" gutterBottom>
-              body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore
-              consectetur, neque doloribus, cupiditate numquam dignissimos
-              laborum fugiat deleniti? Eum quasi quidem quibusdam.
-            </Typography>
-            <Typography variant="button" gutterBottom sx={{ display: 'block' }}>
-              button text
-            </Typography>
-            <Typography
-              variant="caption"
-              gutterBottom
-              sx={{ display: 'block' }}
-            >
-              caption text
-            </Typography>
-            <Typography
-              variant="overline"
-              gutterBottom
-              sx={{ display: 'block' }}
-            >
-              overline text
-            </Typography>
+          <TypographySection />
+          <Divider sx={{ my: 2 }} />
 
-            <Divider />
+          <Box>
             <Typography variant="h2">Text Fields</Typography>
             <TextField
               label="Variant"
